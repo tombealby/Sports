9a1915c88c363ae04042c8eb0a9733fdb2d9dbbf
Tom Bealby <tom.bealby67@gmail.com>
2018-07-24 07:53:22 +0100

    Set results from string input in Game Harness.

--- src/model/games/GameHarness.java
+++ src/model/games/GameHarness.java
@@ -1,31 +1,22 @@
 package model.games;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
-
 import model.points.PointsCalculatorFootballMyTeam;
 
 public class GameHarness {
 
 	public static void main(String[] args) throws Exception {
 
+		String inputString = "4-1A,2-1H,1-1H,4-5A,0-1H";
+		String[] results = inputString.split(",");
+
 		PointsCalculatorFootballMyTeam pcMyTeam = new PointsCalculatorFootballMyTeam();
 
 		int points = 0;
-		for (String inputResult: getInputResults()) {
+		for (String inputResult: results) {
 			points += pcMyTeam.getPointsForMyTeam(new FootballGameInvolvingMyTeam(inputResult, "DTS United"));
 		}
 		System.out.println("points: " + points);
 
-	}
-	
-	private static List<String> getInputResults() {
-		List<String> inputResults = new ArrayList<String>();
-		inputResults.add("4-1A");
-		inputResults.add("2-1H");
-		inputResults.add("1-1H");
-		return inputResults;
 	}
 
 }
