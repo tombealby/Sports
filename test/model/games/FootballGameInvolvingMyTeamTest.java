34fb23d4b5aebf1ffaee1fc2d7eb6b9b9942504d
Tom Bealby <tom.bealby67@gmail.com>
2018-07-24 06:15:08 +0100

    Football results (8) : Tests for football games.

---
+++ test/model/games/FootballGameInvolvingMyTeamTest.java
@@ -0,0 +1,28 @@
+package model.games;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import model.results.Result;
+
+public class FootballGameInvolvingMyTeamTest {
+
+	@Test
+	public void test_getResultWhenDraw() throws Exception{
+		FootballGameInvolvingMyTeam footballGameInvolvingMyTeam = new FootballGameInvolvingMyTeam("88-88H", "DTS United");
+		Assert.assertEquals(Result.DRAW, footballGameInvolvingMyTeam.getResult());
+	}
+
+	@Test
+	public void test_invalidInputString() {
+		FootballGameInvolvingMyTeam footballGameInvolvingMyTeam = null;
+		try {
+			footballGameInvolvingMyTeam = new FootballGameInvolvingMyTeam("88-88", "DTS United");
+			Assert.fail("Exception expected due to invalid input => should not reach this line.");
+		} catch (Exception e) {
+			// Exception is expected => if code executes catch block then the test has passed.
+		}
+		Assert.assertNull(footballGameInvolvingMyTeam);
+	}
+
+}
